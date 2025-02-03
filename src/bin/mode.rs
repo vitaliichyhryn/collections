08e36b0156bf62d2ad45c26328fbd5199f1c4b31
Chyhryn Vitalii <chyhryn.vitalii@gmail.com>
2025-02-03 22:02:53 +0200

    refactor mode

--- src/bin/mode.rs
+++ src/bin/mode.rs
@@ -2,18 +2,22 @@ use std::collections::HashMap;
 use std::env;
 
 fn mode(v: &[i32]) -> i32 {
-    let mut map = HashMap::new();
-    for value in v {
-        let count = map.entry(*value).or_insert(0);
+    let mut frequency = HashMap::new();
+    for &value in v {
+        let count = frequency.entry(value).or_insert(0);
         *count += 1;
     }
+
     let mut mode = v[0];
-    for (key, value) in &map {
-        let mode_count = map.get(&mode).copied().unwrap_or(0);
-        if *value > mode_count {
-            mode = *key;
+    let mut mode_count = frequency.get(&mode).copied().unwrap();
+
+    for (&value, &count) in &frequency {
+        if count > mode_count {
+            mode = value;
+            mode_count = count;
         }
     }
+
     mode
 }
 
